72518bfaabe25e542a1348ee5aac5f7b9a0432d7
Flavio Ribeiro <flavioribeiro.tech@gmail.com>
2024-08-07 12:31:40 -0300

    feat(astroui): add loading icon

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -4,6 +4,20 @@ local M = {
   ---@type AstroUIOpts
   opts = {
     colorscheme = "catppuccin",
+    -- Icons can be configured throughout the interface
+    icons = {
+      -- configure the loading of the lsp in the status line
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
   },
 }
 
